41e7371b5e22777e58aaada3e6c31192e721e66a
saiyam rajput <141070294+Rajputsaiyam@users.noreply.github.com>
2025-07-11 16:41:43 +0530

    commit

--- Part-4/lib/validators/movie.validators.js
+++ Part-4/lib/validators/movie.validators.js
@@ -2,7 +2,7 @@ const {z} = require('zod');
 
 const createMovieValidationSchema = z.object({
     title:z.string().min(3).max(25),
-    description:z.string().min(3).max(25),
+    description:z.string().min(3),
     language:z.string(),
     duration:z.string(),
     coverImageURL:z.string(),
